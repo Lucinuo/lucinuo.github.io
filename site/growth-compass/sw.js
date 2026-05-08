45ff718a3a4d633262c328633c385a539583d937
Lucille <126996419+Lucinuo@users.noreply.github.com>
2026-05-08 10:55:38 +0800

    fix: bump cache to v6, skipWaiting + clients.claim for instant update

--- site/growth-compass/sw.js
+++ site/growth-compass/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "growth-compass-v5";
+const CACHE_NAME = "growth-compass-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -14,13 +14,14 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
